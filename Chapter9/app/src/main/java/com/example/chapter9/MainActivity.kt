553559d4a12ce23ac170d4a409d157c0ac9d0c02
sohee <soheeaubrey@ewha.ac.kr>
2025-03-07 02:34:00 +0900

    .

---
+++ Chapter9/app/src/main/java/com/example/chapter9/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.chapter9
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.chapter9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay()}
+        binding.pauseButton.setOnClickListener { mediaPlayerPause()}
+        binding.stopButton.setOnClickListener { mediaPlayerStop()}
+
+
+    }
+
+    private fun mediaPlayerPlay() {
+
+    }
+
+    private fun mediaPlayerPause() {
+
+    }
+
+    private fun mediaPlayerStop() {
+
+    }
+}
